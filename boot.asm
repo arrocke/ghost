eaeebd9aa09facd670c6f7aace20a45293732c87
Adrian Rocke <me@adrianrocke.com>
2025-01-13 15:49:10 -0600

    feat: use org directive to simplify addresses

--- boot.asm
+++ boot.asm
@@ -1,17 +1,8 @@
+[org 0x7c00]
+
 mov ah, 0x0e            ; scrolling teletype BIOS routine
 
-mov al, the_secret      ; this won't work because `the_secret` is an offset, not the contents of the offset
-int 0x10
-
-mov al, [the_secret]    ; this will print the contents of the address, but still won't work because
-int 0x10                ; the offset is treated here as from the beginning of memory rather than from where the boot sector is in memory
-
-mov bx, the_secret      ; this works because we have added the offset to the memory location of the boot sector
-add bx, 0x7c00          ; before we read the contents of the address
-mov al, [bx]
-int 0x10
-
-mov al, [0x7c1d]        ; this also works because we precomputed the calculation
+mov al, [the_secret]    ; this address is relative to the addres in the org directive
 int 0x10
 
 jmp $                   ; Infinite loop, the "os"
