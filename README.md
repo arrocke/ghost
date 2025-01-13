26ea22454c15a5e15e32e4c89cd7d383e104cee6
Adrian Rocke <me@adrianrocke.com>
2025-01-12 21:37:02 -0600

    feat: print to the screen on boot

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Ghost
 
+At this point the bootloader should print "hello" and then loop forever
+
 Compile the boot sector: `nasm boot.asm -f bin -o ghost.bin`
-Run on emulator: `qemu ghost.bin`
+Run on emulator: `bochs -q`
--- boot.asm
+++ boot.asm
@@ -1,5 +1,21 @@
-loop:                   ; Infinite loop, the "os"
-    jmp loop
+mov ah, 0x0e            ; scrolling teletype BIOS routine
+
+mov al, 'H'             ; character to print when invoking interrupt 0x10
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'o'
+int 0x10
+
+jmp $                   ; Infinite loop, the "os"
 
 times 510-($-$$) db 0   ; Pad 0s until the 510th byte
 
