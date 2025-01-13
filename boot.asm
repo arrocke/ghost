820db1c91bceff80b820acb58b2a1009d6224006
Adrian Rocke <me@adrianrocke.com>
2025-01-13 15:44:35 -0600

    feat: load bytes from program data

--- boot.asm
+++ boot.asm
@@ -1,21 +1,23 @@
 mov ah, 0x0e            ; scrolling teletype BIOS routine
 
-mov al, 'H'             ; character to print when invoking interrupt 0x10
+mov al, the_secret      ; this won't work because `the_secret` is an offset, not the contents of the offset
 int 0x10
 
-mov al, 'e'
+mov al, [the_secret]    ; this will print the contents of the address, but still won't work because
+int 0x10                ; the offset is treated here as from the beginning of memory rather than from where the boot sector is in memory
+
+mov bx, the_secret      ; this works because we have added the offset to the memory location of the boot sector
+add bx, 0x7c00          ; before we read the contents of the address
+mov al, [bx]
 int 0x10
 
-mov al, 'l'
-int 0x10
-
-mov al, 'l'
-int 0x10
-
-mov al, 'o'
+mov al, [0x7c1d]        ; this also works because we precomputed the calculation
 int 0x10
 
 jmp $                   ; Infinite loop, the "os"
+
+the_secret:
+    db "X"
 
 times 510-($-$$) db 0   ; Pad 0s until the 510th byte
 
