4da7c14235f0777471e6951e2f6053c0c94c1cd6
Adrian Rocke <me@adrianrocke.com>
2025-01-15 13:19:05 -0600

    feat: compile and run C kernel

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Ghost
 
-At this point the bootloader should print "hello" and then loop forever
+At this point the bootloader should get to the C kernel and print an X in the top left corner
 
-Compile the boot sector: `nasm boot.asm -f bin -o ghost.bin`
+Compile the boot sector: `nasm boot.asm -f bin -o boot.bin`
+Compile the kernel object file: `gcc -ffreestanding -c kernel.c -o kernel.o`
+Compile the kernel_entry object file: `nasm kernel_entry.asm -f elf64 -o kernel_entry.o`
+Link the kernel entry and kernel object files: `ld -o kernel.bin -T linker.ld kernel_entry.o kernel.o --oformat binary`
+Append kernel to bootloader: `cat boot.bin kernel.bin > ghost.img`
 Run on emulator: `bochs -q`
--- boot.asm
+++ boot.asm
@@ -1,10 +1,15 @@
 [org 0x7c00]
+KERNEL_OFFSET equ 0x1000    ; The memory address where the kernel will be loaded
 
-    mov bp, 0x8000          ; Initialize the stack out of the way
+    mov [BOOT_DRIVE], dl    ; Save this so we know where to load the kernel from
+
+    mov bp, 0x9000          ; Initialize the stack out of the way
     mov sp, bp
 
     mov ax, MSG_REAL_MODE
     call print_string
+
+    call load_kernel
 
     call switch_to_pm       ; Enter protected mode
 
@@ -13,14 +18,32 @@
 %include "print.asm"
 %include "gdt.asm"
 %include "pm.asm"
+%include "disk.asm"
 
+[bits 16]
+load_kernel:
+    mov ax, MSG_LOAD_KERNEL
+    call print_string
+
+    mov bx, KERNEL_OFFSET   ; Load into the kernel address
+    mov dh, 15              ; Load 15 sectors to give us room for the kernel to grow
+    mov dl, [BOOT_DRIVE]    ; Load from the saved boot drive
+    call disk_load
+
+    ret
+
+[bits 32]
 BEGIN_PM:
     mov ebx, MSG_PROT_MODE
     call print_string_pm
 
+    call KERNEL_OFFSET
+
     jmp $
 
+BOOT_DRIVE db 0
 MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_LOAD_KERNEL db "Loaindg kernel into memory.", 0
 MSG_PROT_MODE db "Successfully landed in 32 - bit Protected Mode", 0
 
 ; Boot sector detection
